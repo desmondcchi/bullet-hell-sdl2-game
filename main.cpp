edf359d0e60cd31804e627f23e685290c4d020a9
Desmond Chi <dcchi@live.com>
2022-12-14 03:06:52 -0800

    Created a basic SDL2 program that opens a window.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
-include 
+#include "SDL2/SDL.h"
+
+int main() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow("Bullet Hell Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                isRunning = false;
+            }
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
